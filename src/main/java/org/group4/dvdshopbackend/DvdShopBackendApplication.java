29ad9a2c860019a2ab12166cb8de08ddf4613607
JUNO <mbca.juno@gmail.com>
2025-08-12 20:38:50 +0900

    build.gradle 순서 정리 SpringBoot Application 실행시 JpaAuditing 활성화 어노테이션 추가

--- src/main/java/org/group4/dvdshopbackend/DvdShopBackendApplication.java
+++ src/main/java/org/group4/dvdshopbackend/DvdShopBackendApplication.java
@@ -2,8 +2,10 @@ package org.group4.dvdshopbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DvdShopBackendApplication {
 
 	public static void main(String[] args) {
