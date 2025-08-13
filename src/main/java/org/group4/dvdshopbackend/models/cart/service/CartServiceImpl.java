1987d7e94bc524e21b912422e02e0883aab06e6b
JUNO <mbca.juno@gmail.com>
2025-08-13 10:04:46 +0900

    Cart/ addCart

--- src/main/java/org/group4/dvdshopbackend/models/cart/service/CartServiceImpl.java
+++ src/main/java/org/group4/dvdshopbackend/models/cart/service/CartServiceImpl.java
@@ -1,6 +1,8 @@
 package org.group4.dvdshopbackend.models.cart.service;
 
 import lombok.RequiredArgsConstructor;
+import org.group4.dvdshopbackend.common.entity.Cart;
+import org.group4.dvdshopbackend.common.entity.CartItem;
 import org.group4.dvdshopbackend.models.cart.dto.addItem.AddItemReq;
 import org.group4.dvdshopbackend.models.cart.dto.addItem.AddItemRes;
 import org.group4.dvdshopbackend.models.cart.dto.getCartList.GetCartListRes;
@@ -10,6 +12,7 @@ import org.group4.dvdshopbackend.models.cart.dto.removeItem.RemoveItemReq;
 import org.group4.dvdshopbackend.models.cart.dto.removeItem.RemoveItemRes;
 import org.group4.dvdshopbackend.models.cart.repository.CartItemRepository;
 import org.group4.dvdshopbackend.models.cart.repository.CartRepository;
+import org.group4.dvdshopbackend.models.member.repository.MemberJpaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +23,8 @@ public class CartServiceImpl implements CartService {
 	private final CartRepository cartRepository;
 	private final CartItemRepository cartItemRepository;
 
+	private final MemberJpaRepository memberJpaRepository;
+
 	@Override
 	@Transactional
 	public AddItemRes addItem(AddItemReq req) {
@@ -27,12 +32,42 @@ public class CartServiceImpl implements CartService {
 		var itemId = req.getItemId();
 
 		// 1. 멤버 조회
-//		memberRepository
+		var member = memberJpaRepository.findById(memberEmail)
+				.orElseThrow();
 
 		// 2. 카트 조회 후 없으면 생성
-//		cartRepository.
+		var cart = cartRepository.findByMemberId(member.getId());
 
-		return null;
+		if (cart == null) {
+			var newCart = new Cart();
+			newCart.setMember(member);
+
+			cart = cartRepository.save(newCart);
+		}
+
+		// 3. cart item 추가
+		// cart ID + item ID로 cart 조회
+
+		var cartItem = cartItemRepository.findCartItemByCartIdAndItemId(cart.getId(), itemId)
+				.orElseThrow();
+
+		if (cartItem == null) {
+			// 추가
+			var newCartItem = new CartItem();
+			newCartItem.setCart(cart);
+			newCartItem.setCount(req.getItemCount());
+			cartItemRepository.save(newCartItem);
+		} else {
+			// 수정
+			cartItem.setCount(cartItem.getCount() + 1);
+		}
+
+		return AddItemRes.builder()
+				.cartItemId(cartItem.getId())
+				.cartId(cart.getId())
+				.itemId(itemId)
+				.itemCount(cartItem.getCount())
+				.build();
 	}
 
 	@Override
