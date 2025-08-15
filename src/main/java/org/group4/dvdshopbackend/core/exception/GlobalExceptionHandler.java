d604919ea5f250726fcaf547b89ea1b3f7a4cbb5
JUNO <mbca.juno@gmail.com>
2025-08-15 13:10:30 +0900

    swagger 추가 build gradle QueryDSL용 Q객체 갱신로직 개선 모니터링용 패키지 의존성 추가 (일단)

--- src/main/java/org/group4/dvdshopbackend/core/exception/GlobalExceptionHandler.java
+++ src/main/java/org/group4/dvdshopbackend/core/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.group4.dvdshopbackend.core.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.group4.dvdshopbackend.core.api.ApiError;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,7 +10,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ApiError handleAllExceptions(Exception e) {
+    public ApiError handleAllExceptions(HttpServletRequest req, Exception e) throws Exception {
+
+        String uri = req.getRequestURI();
+        if (uri.startsWith("/v3/api-docs") || uri.startsWith("/swagger-ui")) {
+            throw e;
+        }
+
         var status = HttpStatus.INTERNAL_SERVER_ERROR.value();
         var message = e.getMessage();
 
